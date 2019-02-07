dcf68367f2b1c56c5eea08ca240acb622aa56810
= <=>
2019-02-07 17:28:31 -0300

    Entidade Atividade feita

---
+++ src/br/com/educamais/model/Atividade.java
@@ -0,0 +1,53 @@
+package br.com.educamais.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="atividade")
+public class Atividade {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_atividade")
+	private int idAtividade;
+	
+	@Column (name = "nome_atividade")
+	private String nomeAtividade;
+	
+	@Column (name = "data_atividade")	
+	private Date dataAtividade;
+
+	public int getIdAtividade() {
+		return idAtividade;
+	}
+
+	public void setIdAtividade(int idAtividade) {
+		this.idAtividade = idAtividade;
+	}
+
+	public String getNomeAtividade() {
+		return nomeAtividade;
+	}
+
+	public void setNomeAtividade(String nomeAtividade) {
+		this.nomeAtividade = nomeAtividade;
+	}
+
+	public Date getDataAtividade() {
+		return dataAtividade;
+	}
+
+	public void setDataAtividade(Date dataAtividade) {
+		this.dataAtividade = dataAtividade;
+	}
+	
+	
+	
+	
+}
